097d2621aaa0daa571a566d51685e623e157cd08
DenisCDev <coldgreen04@gmail.com>
2022-05-11 08:14:18 -0300

    Corrigindo model

--- src/main/java/com/br/coldtech/filmes/model/Filme.java
+++ src/main/java/com/br/coldtech/filmes/model/Filme.java
@@ -1,4 +1,4 @@
-package br.com.tech4me.coldtech.model;
+package com.br.coldtech.filmes.model;
 
 import org.springframework.data.annotation.Id;
 
@@ -18,5 +18,70 @@ public class Filme {
     private int nota;
     private int duracaoMinutos;
 
-    //Criar getters e setters
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+    public String getSinopse() {
+        return sinopse;
+    }
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+    public String getDiretor() {
+        return diretor;
+    }
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+    public String getProdutor() {
+        return produtor;
+    }
+    public void setProdutor(String produtor) {
+        this.produtor = produtor;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public String getClassificacao() {
+        return classificacao;
+    }
+    public void setClassificacao(String classificacao) {
+        this.classificacao = classificacao;
+    }
+    public String getIdiomas() {
+        return idiomas;
+    }
+    public void setIdiomas(String idiomas) {
+        this.idiomas = idiomas;
+    }
+    public int getNota() {
+        return nota;
+    }
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
 }
