729ca1631d0b64ae35e050002430032084182065
DenisCDev <coldgreen04@gmail.com>
2022-06-04 19:57:59 -0300

    Service com funções básicas

---
+++ src/main/java/com/br/coldtech/filmes/service/FilmeService.java
@@ -0,0 +1,22 @@
+package com.br.coldtech.filmes.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.br.coldtech.filmes.dto.FilmeDTO;
+
+public interface FilmeService {
+    
+    List<FilmeDTO> obterTodosFilmes();
+
+    Optional<FilmeDTO> obterFilmePorId(String id);
+
+    FilmeDTO cadastrarFilme(FilmeDTO filme);
+
+    void excluirFilmePorId(String id);
+
+    FilmeDTO atualizarInfoFilme(String id, FilmeDTO filme);
+
+    List<FilmeDTO> obterPorNome(String nome);
+
+}
