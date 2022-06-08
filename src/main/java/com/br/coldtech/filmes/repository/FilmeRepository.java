5af108830bd87276e78e66c17655527d28138141
Denis Scarabelli <87149177+DenisCDev@users.noreply.github.com>
2022-06-08 00:23:11 -0300

    Delete src directory

--- src/main/java/com/br/coldtech/filmes/repository/FilmeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.coldtech.filmes.repository;
-
-import com.br.coldtech.filmes.model.Filme;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FilmeRepository extends MongoRepository<Filme, String>{
-}
